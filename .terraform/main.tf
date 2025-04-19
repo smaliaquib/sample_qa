45767d8a756bf8aa0348c50de045cb8ee9026318
Aquib Ali Khan <aquibalicool@yahoo.in>
2025-04-19 19:35:47 -0400

    Update main.tf

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -8,6 +8,8 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.32"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
     default = {
@@ -17,9 +19,14 @@ module "eks" {
       instance_types = ["t3.medium"]
     }
   }
+}
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "eks-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
