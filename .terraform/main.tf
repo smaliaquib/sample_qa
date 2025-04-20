1b914a251da6df1797b74c4fc103cb0a2bdf662c
Aquib Ali Khan <aquibalicool@yahoo.in>
2025-04-19 20:40:05 -0400

    Update main.tf

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -10,7 +10,6 @@ module "eks" {
   cluster_name = "${var.eks_cluster_name}-${var.project_id}"
   vpc_id = var.vpc_id
   subnet_ids = var.subnet_ids
-  node_instance_type = var.node_instance_type
   node_desired_size = var.node_desired_size
   node_max_size = var.node_max_size
   node_min_size = var.node_min_size
